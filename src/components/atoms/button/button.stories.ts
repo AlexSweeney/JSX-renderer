3bd0d8aba0fe92807ad7883ccf84f54d64c66ec3
Alex Sweeney <a.sweeney@ryte.com>
2025-10-04 16:13:27 +0100

    add storybook

---
+++ src/components/atoms/button/button.stories.ts
@@ -0,0 +1,13 @@
+import { Button } from './button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const Primary = {
+  args: {},
+};
