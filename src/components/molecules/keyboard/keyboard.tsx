98a11948caa32b4263d05cb65e9ef7c5a4fb83dd
Alex Sweeney <a.sweeney@ryte.com>
2025-10-05 18:34:51 +0100

    add html buttons to keyboard

--- src/components/molecules/keyboard/keyboard.tsx
+++ src/components/molecules/keyboard/keyboard.tsx
@@ -3,25 +3,36 @@ import { Button } from '../../atoms/button/button';
 const rowOne = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
 const rowTwo = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
 const rowThree = ["z", "x", "c", "v", "b", "n", "m"];
+const rowFour = ["<h1>", "</h1>", "<p>", "</p>"];
+
+const keyCodes = {
+  parse: 'ShiftLeft',
+  render: 'ShiftRight',
+  "<h1>": 'AltLeft',
+  "</h1>": 'MetaLeft',
+  "<p>": 'MetaRight',
+  "</p>": 'AltRight',
+};
 
 export interface KeyboardProps {
   onClick: (event: any) => void;
   onKeyDown: (event: WindowEventMap['keydown']) => void;
+  className?: string;
 }
 
-export const Keyboard = ({ onClick, onKeyDown, }: KeyboardProps) => {
+export const Keyboard = ({ onClick, onKeyDown, className }: KeyboardProps) => {
   const rowClass = 'mb-2 flex justify-center';
-  const characterButtonClass = 'mr-2 last:mr-0';
+  const buttonClass = 'mr-2 last:mr-0';
 
   const characterButtonProps = {
     variant: 'character' as const,
-    className: characterButtonClass,
+    className: buttonClass,
     onClick,
     onKeyDown,
   };
 
   return (
-    <div className='bg-surface p-2 rounded'>
+    <div className={`bg-surface p-2 rounded ${className}`}>
       <div className={rowClass}>
         {rowOne.map(char => (
           <Button {...characterButtonProps} character={char} key={char} />
@@ -33,15 +44,42 @@ export const Keyboard = ({ onClick, onKeyDown, }: KeyboardProps) => {
         ))}
       </div>
       <div className={rowClass}>
-        <Button variant="action" text="parse" className="mr-2" keyCode="ShiftLeft" />
+        <Button variant="action" text="parse" className="mr-2" keyCode={keyCodes.parse} />
         {rowThree.map(char => (
           <Button {...characterButtonProps} character={char} key={char} />
         ))}
-        <Button variant="action" text="render" keyCode="ShiftRight" />
+        <Button variant="action" text="render" keyCode={keyCodes.render} />
       </div>
-      <div className={`${rowClass} mb-0 relative w-full px-2`}>
+      <div className={`${rowClass} relative w-full px-2`}>
         <Button variant='space' />
         <Button variant="delete" className="absolute right-0" />
+      </div>
+      <div className='flex justify-between'>
+        {/* Left-side buttons */}
+        <div className="flex">
+          {rowFour.slice(0, -2).map((char) => (
+            <Button
+              key={char}
+              keyCode={keyCodes[char as keyof typeof keyCodes] || ''}
+              variant="html"
+              text={char}
+              className={buttonClass}
+            />
+          ))}
+        </div>
+
+        {/* Right-side button */}
+        <div className="flex">
+          {rowFour.slice(-2).map((char) => (
+            <Button
+              key={char}
+              keyCode={keyCodes[char as keyof typeof keyCodes] || ''}
+              variant="html"
+              text={char}
+              className={buttonClass}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
