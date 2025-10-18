bf3855cff56cc4844ddbcb56597d56cbb2aaa53f
Alex Sweeney <a.sweeney@ryte.com>
2025-10-18 18:31:32 +0100

    add code-input component + story

---
+++ src/components/organisms/code-input/code-input.stories.tsx
@@ -0,0 +1,16 @@
+import { CodeInput } from './code-input';
+
+export default {
+  title: 'Organisms/CodeInput',
+  component: CodeInput,
+  parameters: {
+    layout: 'centered',
+  },
+  globals: {
+    backgrounds: { value: 'dark' },
+  },
+};
+
+export const Primary = {
+
+}
---
+++ src/components/organisms/code-input/code-input.tsx
@@ -0,0 +1,32 @@
+import React, { useState, MouseEvent } from "react";
+import { Keyboard } from "../../molecules/keyboard/keyboard";
+import { Screen } from "../../atoms/screen/screen";
+import { deleteFromString } from "../../molecules/keyboard/utils/keyboard-utils";
+
+export const CodeInput = () => {
+  const [code, setCode] = useState<string>('');
+
+  const handleKeyDown = (event: WindowEventMap['keydown'], value: string) => {
+    if (event.code === 'Backspace') {
+      setCode((prev) => deleteFromString(prev));
+    } else {
+      setCode((prev) => prev + value);
+    }
+  };
+
+  const handleKeyClick = (_: MouseEvent, value: string) => {
+    if (value === 'delete') {
+      setCode((prev) => deleteFromString(prev));
+    } else {
+      setCode((prev) => prev + value);
+    }
+  };
+
+  return <div className="grid grid-rows-2 items-stretch border border-primary rounded p-2 gap-2">
+    <Screen code={code} />
+    <div className="flex">
+      <Keyboard onClick={handleKeyClick} onKeyDown={handleKeyDown} inputString={code} />
+      {/* <NumPad /> */}
+    </div>
+  </div>
+};
