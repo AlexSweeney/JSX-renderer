efd5defb369d079adcfb79ad44d6890818a09a09
Alex Sweeney <a.sweeney@ryte.com>
2025-11-05 21:48:20 +0000

    refactor keyboard - use flex grid

--- src/components/molecules/keyboard/keyboard.tsx
+++ src/components/molecules/keyboard/keyboard.tsx
@@ -2,10 +2,10 @@ import React, { MouseEvent } from 'react';
 import { Button } from '../../atoms/button/button';
 import { getDisabledSections, getIsOpeningTag } from './utils/keyboard-utils';
 
-const topLetters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
-const middleLetters = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
-const bottomLetters = ["z", "x", "c", "v", "b", "n", "m"];
-const htmlTags = ["<h1>", "</h1>", "<p>", "</p>"];
+const HTML_TAGS = ["<h1>", "</h1>", "<p>", "</p>"];
+const TOP_LETTERS = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
+const MIDDLE_LETTERS = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
+const BOTTOM_LETTERS = ["z", "x", "c", "v", "b", "n", "m"];
 
 const keyCodes = {
   parse: 'ShiftLeft',
@@ -41,86 +41,77 @@ export const Keyboard = ({
     disabled: charactersDisabled,
   };
 
+  const renderHtmlButtons = () => {
+    return (
+      <div>
+        {HTML_TAGS.map((tag) => {
+          const tagType = getIsOpeningTag(tag) ? 'opening' : 'closing';
+
+          let isDisabled = false;
+          if (tagType === 'opening') {
+            isDisabled = openingTagsDisabled;
+          }
+          if (tagType === 'closing') {
+            isDisabled = closingTagsDisabled || tag !== validClosingTag;
+          }
+
+          return (<Button
+            key={tag}
+            keyCode={keyCodes[tag as keyof typeof keyCodes] || ''}
+            variant="html"
+            text={tag}
+            className={buttonClass}
+            disabled={isDisabled}
+            onClick={(e) => onClick(e, tag)}
+            onKeyDown={(e) => onKeyDown(e, tag)}
+          />)
+        })}
+      </div>
+    );
+  };
+
+  const renderLetterButtons = (letters: string[]) => {
+    return (letters.map(letter => (
+      <Button
+        {...characterButtonProps}
+        onClick={(e) => onClick(e, letter)}
+        onKeyDown={(e) => onKeyDown(e, letter)}
+        character={letter}
+        key={letter}
+      />
+    )));
+  }
+
   return (
-    <div className={`bg-surface p-2 rounded ${className}`}>
-      <div className={`${rowClass} !justify-between`}>
-        <div>
-          {htmlTags.map((tag) => {
-            const tagType = getIsOpeningTag(tag) ? 'opening' : 'closing';
-
-            let isDisabled = false;
-            if (tagType === 'opening') {
-              isDisabled = openingTagsDisabled;
-            }
-            if (tagType === 'closing') {
-              isDisabled = closingTagsDisabled || tag !== validClosingTag;
-            }
-
-            return (<Button
-              key={tag}
-              keyCode={keyCodes[tag as keyof typeof keyCodes] || ''}
-              variant="html"
-              text={tag}
-              className={buttonClass}
-              disabled={isDisabled}
-              onClick={(e) => onClick(e, tag)}
-              onKeyDown={(e) => onKeyDown(e, tag)}
-            />)
-          })}
-        </div>
+    <div className='bg-surface p-2 grid grid-rows-5 gap-2'>
+      <div className='flex justify-between'>
+        {renderHtmlButtons()}
         <Button
           variant="delete"
           onClick={(e) => onClick(e, 'delete')}
           onKeyDown={(e) => onKeyDown(e, 'delete')}
         />
       </div>
-      <div className={rowClass}>
-        {topLetters.map(char => (
-          <Button
-            {...characterButtonProps}
-            onClick={(e) => onClick(e, char)}
-            onKeyDown={(e) => onKeyDown(e, char)}
-            character={char}
-            key={char}
-          />
-        ))}
+      <div>
+        {renderLetterButtons(TOP_LETTERS)}
       </div>
-      <div className={rowClass}>
-        {middleLetters.map(char => (
-          <Button
-            {...characterButtonProps}
-            onClick={(e) => onClick(e, char)}
-            onKeyDown={(e) => onKeyDown(e, char)}
-            character={char}
-            key={char}
-          />
-        ))}
+      <div className="flex justify-center">
+        {renderLetterButtons(MIDDLE_LETTERS)}
       </div>
-      <div className={rowClass}>
+      <div className='flex justify-between'>
         <Button variant="action" text="parse" disabled={parseDisabled} className="mr-2" keyCode={keyCodes.parse} />
-        {bottomLetters.map(char => (
-          <Button
-            {...characterButtonProps}
-            onClick={(e) => onClick(e, char)}
-            onKeyDown={(e) => onKeyDown(e, char)}
-            character={char}
-            key={char}
-          />
-        ))}
-
+        {renderLetterButtons(BOTTOM_LETTERS)}
         <Button variant="action" text="render" disabled={renderDisabled} keyCode={keyCodes.render} />
       </div>
-      <div className={`${rowClass}flex w-full items-center justify-between`}>
-        <div className='flex-1 flex justify-center'>
-          <Button
-            variant='space'
-            onClick={(e) => onClick(e, ' ')}
-            onKeyDown={(e) => onKeyDown(e, ' ')}
-          />
-        </div>
-
-        <Button variant="action" text="enter" color="secondary" />
+      <div className='grid grid-cols-[1fr_2fr_1fr]'>
+        <Button
+          variant='space'
+          onClick={(e) => onClick(e, ' ')}
+          onKeyDown={(e) => onKeyDown(e, ' ')}
+          className="col-start-2"
+        />
+        <Button variant="action" text="enter" color="secondary" className="col-start-3 justify-self-end" />
       </div>
     </div>
-  )
+  );
 }
