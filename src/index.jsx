b8c1b185264c5c4b9e5128e22fbd68d78490470d
Alex Sweeney <a.sweeney@ryte.com>
2025-11-14 18:28:18 +0000

    wip

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,56 @@
-function render(node) {
-  const jsxOutput = document.getElementById('jsxOutputText');
-  jsxOutput.innerHTML = node;
+import './styles.css';
+
+function transpile(nodeName, attributes, ...args) {
+  const children = args.length ? [].concat(...args) : null;
+  return { nodeName, attributes, children };
+}
+
+function parseJsxString(jsxString) {
+  const jsxContent = /\{([^}]+)\}/g;
+  const matches = [...jsxString.matchAll(regex)];
+  const contents = matches.map(match => match[1]);
+
+  if (contents) {
+    console.log('contents', contents)
+  } else {
+    console.log('no match')
+  }
+
+  // return null;
+}
+
+const jsxString = '<div id="foo">Hello!</div>';
+// const vdom = transpile(jsx);
+
+// console.log('vdom:', vdom)
+
+// function makeVdom(node) {
+//   const textArray = node.split(' ');
+
+
+//   let jsx = (
+//     <div id="vdom">
+//       <ul>{textArray.map(el => <li>{el}</li>)}</ul>
+//     </div>
+//   );
+
+//   const vdom = transpile(jsx);
+
+//   console.log('vdom', vdom)
+//   return vdom;
+// }
+
+function render(jsxString) {
+  const parsedJsxString = parseJsxString(jsxString);
+
+
+  // compile jsx to js
+  // const vdom = makeVdom(node);
+
+  // // create vdom
+
+  // // render to dom
+  // document.body.appendChild(vdom);
 }
 
 window.render = render;
