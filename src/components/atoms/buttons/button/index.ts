85f214447ba2d0ba6b4dcd76e82f41fc981506b6
Alex Sweeney <a.sweeney@ryte.com>
2025-11-23 17:46:03 +0000

    fix error - import types as type

---
+++ src/components/atoms/buttons/button/index.ts
@@ -0,0 +1,5 @@
+import { Button, BUTTON_COLORS } from './Button';
+import type { ButtonProps, ButtonColorType, ButtonState } from './Button';
+
+export { Button, BUTTON_COLORS };
+export type { ButtonProps, ButtonColorType, ButtonState };
--- src/components/atoms/buttons/button/index.tsx
+++
@@ -1,3 +0,0 @@
-import { Button, ButtonProps, ButtonColorType, BUTTON_COLORS, ButtonState } from './Button';
-
-export { Button, ButtonProps, ButtonColorType, BUTTON_COLORS, ButtonState };
